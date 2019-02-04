b95c66434e86d538130fb420d29f5eaf126821cd
YuhangYangLyon1 <46898951+YuhangYangLyon1@users.noreply.github.com>
2019-02-04 09:13:15 +0100

    Create CV.js

---
+++ CV.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", function() { //ajoute une fonction efectuée lorsque l'utilisateur scroll sur la page
+
+// Calculez le déplacement ici ...
+var deplacement=(window.scrollY+window.innerHeight)*15/document.body.offsetHeight;
+document.getElementById('progress_bar').value = deplacement; // Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
+});
+
+function myScroll() { // Définition de la fonction de scroll
+    var progress = document.getElementById('progress_bar').value; // Valeur de la barre de progression
+    progress=progress*document.body.offsetHeight/15-window.innerHeight;
+    // Calculez ici la valeur de déplacement à efectuer lors du scroll
+    
+    window.scroll(0,progress) ;// Déplacement de la page de 0 px latéralement et 'value' px horizontalement
+}
